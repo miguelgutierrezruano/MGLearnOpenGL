81fd3d07fdd6445685b2e87f01282aca83636fab
miguelguti <miguelgutierrezruano@gmail.com>
2023-05-01 15:06:51 +0200

    added glad

--- code/main.cpp
+++ code/main.cpp
@@ -5,12 +5,26 @@
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <glad/glad.h>
+
+#include <cassert>
+#include <iostream>
 
 using namespace sf;
 
 int main()
 {
-    Window window(VideoMode(800, 600), "MGLearnOpenGL", Style::Default, ContextSettings(0, 0, 0, 3, 3, ContextSettings::Core));
+    // Window with OpenGL context
+    Window window(VideoMode(800, 600), "MGLearnOpenGL", Style::Default, ContextSettings(0, 0, 0, 4, 4, ContextSettings::Core));
+
+    // Glad initialization
+    GLenum glad_init = gladLoadGL();
+
+    // Stop program if glad could not load properly
+    assert(glad_init != 0);
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
     window.setVerticalSyncEnabled(true);
 
     bool running = true;
