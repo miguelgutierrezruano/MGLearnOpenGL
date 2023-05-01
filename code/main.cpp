1237df6d07cb9b9ba4c9fb113578f76fcad4cc50
miguelguti <miguelgutierrezruano@gmail.com>
2023-05-01 20:46:52 +0200

    super triangle

--- code/main.cpp
+++ code/main.cpp
@@ -12,6 +12,55 @@
 
 using namespace sf;
 
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+
+    glShaderSource (id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+    // Error handling
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex " : "fragment ") << "shader!" << std::endl;
+        std::cout << message << std::endl;
+
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShaderCode, const std::string& fragmentShaderCode)
+{
+    unsigned int program = glCreateProgram();
+
+    unsigned int vertexShader = CompileShader(GL_VERTEX_SHADER, vertexShaderCode);
+    unsigned int fragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderCode);
+
+    glAttachShader(program, vertexShader);
+    glAttachShader(program, fragmentShader);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return program;
+}
+
 int main()
 {
     // Window with OpenGL context
@@ -29,6 +78,57 @@ int main()
 
     bool running = true;
 
+    float coordinates[6] = {
+        -1.0f,  1.0f,
+         0.0f, -1.0f, 
+         1.0f,  1.0f 
+    };
+
+    // Create VAO required in core profile
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // Create vertex buffer on GPU
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, coordinates, GL_STATIC_DRAW);
+
+    // Set up vertex attribute
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, (const void*)0);
+
+    // Shader code
+    std::string vertexShader =
+        R"glsl(
+        #version 330 core
+
+        layout(location = 0) in vec4 position;
+
+        void main() 
+        {
+            gl_Position = position;
+        }
+        )glsl";
+
+    std::string fragmentShader =
+        R"glsl(
+        #version 330 core
+
+        out vec4 color;
+
+        void main()
+        {
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+        )glsl";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
+    glClearColor(0.1f, 0.1f, 0.1f, 1);
+
     do
     {
         Event event;
@@ -45,9 +145,15 @@ int main()
             }
         }
 
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         window.display();
 
     } while (running);
+
+    glDeleteProgram(shader);
 
     return 0;
 }
