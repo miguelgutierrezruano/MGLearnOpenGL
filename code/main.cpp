751857865f921b487c28e447f7eefe7f2ff5e338
miguelguti <miguelgutierrezruano@gmail.com>
2023-05-06 19:39:00 +0200

    shader abstraction

--- code/main.cpp
+++ code/main.cpp
@@ -15,6 +15,7 @@
 #include <string>
 #include <sstream>
 
+#include "Shader.h"
 #include "Renderer.h"
 #include "VertexArray.h"
 #include "VertexBuffer.h"
@@ -25,98 +26,6 @@ using namespace sf;
 using namespace mg;
 
 using namespace std::chrono;
-
-struct ShaderSource
-{
-    std::string vertexSource;
-    std::string fragmentSource;
-};
-
-static ShaderSource ParseShader(const std::string& path)
-{
-    // Open file
-    std::ifstream stream(path);
-
-    enum class ShaderType
-    {
-        NONE = -1, VERTEX = 0, FRAGMENT = 1
-    };
-
-    // Declare string where to copy
-    std::stringstream ss[2];
-    ShaderType type = ShaderType::NONE;
-
-    // Read line from file
-    std::string line;
-    while (getline(stream, line))
-    {
-        // If line defines a shader change type
-        if (line.find("#shader") != std::string::npos)
-        {
-            if (line.find("vertex") != std::string::npos)
-                type = ShaderType::VERTEX;
-            else if (line.find("fragment") != std::string::npos)
-                type = ShaderType::FRAGMENT;
-        }
-        // Add line to shader string
-        else
-        {
-            if(type != ShaderType::NONE)
-                ss[(int)type] << line << '\n';
-        }
-    }
-
-    return { ss[0].str(), ss[1].str() };
-}
-
-static unsigned int CompileShader(unsigned int type, const std::string& source)
-{
-    unsigned int id = glCreateShader(type);
-    const char* src = source.c_str();
-
-    glShaderSource (id, 1, &src, nullptr);
-    glCompileShader(id);
-
-    int result;
-    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
-
-    // Error handling
-    if (result == GL_FALSE)
-    {
-        int length;
-        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-
-        char* message = (char*)alloca(length * sizeof(char));
-        glGetShaderInfoLog(id, length, &length, message);
-
-        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex " : "fragment ") << "shader!" << std::endl;
-        std::cout << message << std::endl;
-
-        glDeleteShader(id);
-        return 0;
-    }
-
-    return id;
-}
-
-static unsigned int CreateShader(const std::string& vertexShaderCode, const std::string& fragmentShaderCode)
-{
-    unsigned int program = glCreateProgram();
-
-    unsigned int vertexShader = CompileShader(GL_VERTEX_SHADER, vertexShaderCode);
-    unsigned int fragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentShaderCode);
-
-    glAttachShader(program, vertexShader);
-    glAttachShader(program, fragmentShader);
-
-    glLinkProgram(program);
-    glValidateProgram(program);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    return program;
-}
 
 int main()
 {
@@ -162,26 +71,21 @@ int main()
     // Create index buffer on GPU
     mg::IndexBuffer indexBuffer(indices, 6);
 
-    // Parse shader code
-    ShaderSource source = ParseShader("../code/shaders/Basic.shader");
-
-    // Compile and use vertex and fragment shader
-    unsigned int shader = CreateShader(source.vertexSource, source.fragmentSource);
-    glUseProgram(shader);
+    mg::Shader shader("../code/shaders/Basic.shader");
+    shader.bind();
+    shader.setUniform4f("u_Color", glm::vec4(1.0f, 0.0f, 1.0f, 1.0f));
 
     glClearColor(0.1f, 0.1f, 0.1f, 1);
 
-    int location = glGetUniformLocation(shader, "u_Color");
-    glUniform4f(location, 1.0f, 0.0f, 1.0f, 1.0f);
 
     float redChannel = 0.0f;
     float increment = 0.05f;
 
     // Unbind every buffer
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    glUseProgram(0);
+    vertexArray.unbind();
+    shader.unbind();
+    vertexBuffer.unbind();
+    indexBuffer.unbind();
 
     // Delta time variables
     auto  chrono = high_resolution_clock();
@@ -209,10 +113,8 @@ int main()
 
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glUseProgram(shader);
-
-        // Set color on main loop
-        glUniform4f(location, redChannel, 0.0f, 1.0f, 1.0f);
+        shader.bind();
+        shader.setUniform4f("u_Color", glm::vec4(redChannel, 0.0f, 1.0f, 1.0f));
 
         vertexArray.bind();
         indexBuffer.bind();
@@ -223,9 +125,9 @@ int main()
         assert(GLLogCall());
 
         if (redChannel > 1.0f)
-            increment = -0.02f;
+            increment = -0.01f;
         else if (redChannel < 0.0f)
-            increment = 0.02f;
+            increment = 0.01f;
 
         redChannel += increment;
 
@@ -242,8 +144,6 @@ int main()
         delta_time = duration<float>(chrono.now() - start).count();
 
     } while (running);
-
-    glDeleteProgram(shader);
 
     return 0;
 }
